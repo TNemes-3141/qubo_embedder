d6b522e3ad8790c7288f41846db9423f1f63bd73
Totemi1324 <nemestomi2@gmail.com>
2023-01-27 00:55:57 +0100

    Extend unit tests

--- test/qubo_embedder_test.dart
+++ test/qubo_embedder_test.dart
@@ -1,16 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:qubo_embedder/qubo_embedder.dart';
-import 'package:tuple/tuple.dart';
+
+import 'package:ml_linalg/linalg.dart';
 
 void main() {
   test('test', () {
-    final hamiltonian = [
-      [0.0, 1.0, 2.0],
-      [0.0, 1.0, 2.0],
-      [0.0, 0.0, 2.0],
-    ];
+    final qubo = Qubo(size: 5);
+    qubo.addEntry(0, 0, value: 0.0);
+    qubo.getEntry(1, 0);
 
-    expect(Utils.hamiltonianToMap(hamiltonian), 0);
+    print(qubo);
   });
 }
