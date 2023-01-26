96d727a3251fb49ea793b91858fc89b03f060fd4
Totemi1324 <nemestomi2@gmail.com>
2023-01-27 00:55:34 +0100

    Add Qubo and Hamiltonian class

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,36 +1,96 @@
 import 'package:tuple/tuple.dart';
+import 'package:ml_linalg/linalg.dart';
 
-class Utils {
-  static Map<Tuple2<int, int>, double> hamiltonianToMap(
-    List<List<double>> hamiltonian,
-  ) {
-    final size = hamiltonian.length;
+import './exceptions.dart';
 
-    for (var rowIndex = 0; rowIndex < hamiltonian.length; rowIndex++) {
-      final row = hamiltonian[rowIndex];
-      assert(row.length == size);
-      for (var i = 0; i < rowIndex; i++) {
-        assert(row[i] == 0);
+class Hamiltonian {
+  final Matrix _matrix;
+
+  Hamiltonian._(this._matrix);
+
+  factory Hamiltonian.fromList(List<List<double>> list) {
+    _checkListFormat(list);
+
+    final matrix = Matrix.fromList(list);
+    return Hamiltonian._(matrix);
+  }
+
+  factory Hamiltonian.fromJson(Map<String, double> json) {
+    final matrix = Matrix.fromJson(json);
+    return Hamiltonian._(matrix);
+  }
+
+  //List<List<double>> get matrix => {
+  //  _matrix.
+  //}
+
+  static void _checkListFormat(List<List<double>> list) {
+    final size = list.length;
+
+    for (var rowIndex = 0; rowIndex < list.length; rowIndex++) {
+      final row = list[rowIndex];
+      if (row.length != size) {
+        throw DataFormattingException(DataFormatError.listNotSquare);
       }
-    }
-
-    final qubo = {for (var e in _generateDiagonal(size)) e: 0.0};
-
-    for (var rowIndex = 0; rowIndex < size; rowIndex++) {
-      for (var columnIndex = 0; columnIndex < size; columnIndex++) {
-        if (hamiltonian[rowIndex][columnIndex] != 0) {
-          qubo[Tuple2(rowIndex, columnIndex)] =
-              hamiltonian[rowIndex][columnIndex];
+      for (var i = 0; i < rowIndex; i++) {
+        if (row[i] != 0) {
+          throw DataFormattingException(
+            DataFormatError.lowerTriangleEntryNotZero,
+          );
         }
       }
     }
+  }
+}
 
-    return qubo;
+class Qubo {
+  final int size;
+  final Map<Tuple2<int, int>, double> _qubo = {};
+
+  Qubo({required this.size});
+
+  void addEntry(
+    int variableIndex,
+    int variablePairIndex, {
+    required double value,
+  }) {
+    if (variableIndex >= size) throw IndexOutOfRangeException("variableIndex");
+    if (variablePairIndex >= size) {
+      throw IndexOutOfRangeException("variableIndex");
+    }
+    if (variablePairIndex < variableIndex) {
+      throw InvalidOperationException(
+        InvalidOperation.pairVariableSmallerThanPrimaryVariable,
+      );
+    }
+
+    final key = Tuple2(variableIndex, variablePairIndex);
+
+    _qubo[key] = value;
   }
 
-  static Iterable<Tuple2<int, int>> _generateDiagonal(int size) sync* {
-    for (var i = 0; i < size; i++) {
-      yield Tuple2(i, i);
+  double getEntry(
+    int variableIndex,
+    int variablePairIndex,
+  ) {
+    final key = Tuple2(variableIndex, variablePairIndex);
+    final element = _qubo[key];
+
+    if (element == null) {
+      throw InvalidOperationException(InvalidOperation.quboEntryDoesNotExist);
     }
+
+    return element;
+  }
+
+  @override
+  String toString() {
+    String content = "{";
+    for (var entry in _qubo.entries) {
+      content += "(${entry.key.item1}, ${entry.key.item2}): ${entry.value}, ";
+    }
+    content += "}";
+
+    return content;
   }
 }
