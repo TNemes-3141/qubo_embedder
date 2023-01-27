50d95c771f2f115c7e59b4f8e36b8a701d4bf6c7
Totemi1324 <nemestomi2@gmail.com>
2023-01-27 23:08:28 +0100

    Implement parsing logic

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -3,46 +3,6 @@ import 'package:ml_linalg/linalg.dart';
 
 import './exceptions.dart';
 
-class Hamiltonian {
-  final Matrix _matrix;
-
-  Hamiltonian._(this._matrix);
-
-  factory Hamiltonian.fromList(List<List<double>> list) {
-    _checkListFormat(list);
-
-    final matrix = Matrix.fromList(list);
-    return Hamiltonian._(matrix);
-  }
-
-  factory Hamiltonian.fromJson(Map<String, double> json) {
-    final matrix = Matrix.fromJson(json);
-    return Hamiltonian._(matrix);
-  }
-
-  //List<List<double>> get matrix => {
-  //  _matrix.
-  //}
-
-  static void _checkListFormat(List<List<double>> list) {
-    final size = list.length;
-
-    for (var rowIndex = 0; rowIndex < list.length; rowIndex++) {
-      final row = list[rowIndex];
-      if (row.length != size) {
-        throw DataFormattingException(DataFormatError.listNotSquare);
-      }
-      for (var i = 0; i < rowIndex; i++) {
-        if (row[i] != 0) {
-          throw DataFormattingException(
-            DataFormatError.lowerTriangleEntryNotZero,
-          );
-        }
-      }
-    }
-  }
-}
-
 class Qubo {
   final int size;
   final Map<Tuple2<int, int>, double> _qubo = {};
@@ -69,28 +29,88 @@ class Qubo {
     _qubo[key] = value;
   }
 
-  double getEntry(
+  double? getEntry(
     int variableIndex,
     int variablePairIndex,
   ) {
     final key = Tuple2(variableIndex, variablePairIndex);
     final element = _qubo[key];
 
-    if (element == null) {
-      throw InvalidOperationException(InvalidOperation.quboEntryDoesNotExist);
-    }
-
     return element;
   }
 
   @override
   String toString() {
-    String content = "{";
+    String content = "[qubits: $size] {";
     for (var entry in _qubo.entries) {
       content += "(${entry.key.item1}, ${entry.key.item2}): ${entry.value}, ";
     }
     content += "}";
 
     return content;
+  }
+}
+
+class Hamiltonian {
+  final Matrix _matrix;
+
+  Hamiltonian._(this._matrix);
+
+  factory Hamiltonian.fromList(List<List<double>> list) {
+    _checkListFormat(list);
+
+    final matrix = Matrix.fromList(list, dtype: DType.float32);
+    return Hamiltonian._(matrix);
+  }
+
+  factory Hamiltonian.fromQubo(Qubo qubo) {
+    final matrix = _quboToMatrix(qubo);
+    return Hamiltonian._(matrix);
+  }
+
+  List<List<double>> get matrix {
+    var flatList = _matrix.asFlattenedList;
+    return List<List<double>>.generate(
+      _matrix.rowCount,
+      (rowIndex) => List<double>.generate(
+        _matrix.columnCount,
+        (columnIndex) => flatList[rowIndex * _matrix.columnCount + columnIndex],
+      ),
+    );
+  }
+
+  static void _checkListFormat(List<List<double>> list) {
+    final size = list.length;
+
+    for (var rowIndex = 0; rowIndex < list.length; rowIndex++) {
+      final row = list[rowIndex];
+      if (row.length != size) {
+        throw DataFormattingException(DataFormatError.listNotSquare);
+      }
+      for (var i = 0; i < rowIndex; i++) {
+        if (row[i] != 0) {
+          throw DataFormattingException(
+            DataFormatError.lowerTriangleEntryNotZero,
+          );
+        }
+      }
+    }
+  }
+
+  static Matrix _quboToMatrix(Qubo qubo) {
+    List<List<double>> rows = [];
+
+    for (var i = 0; i < qubo.size; i++) {
+      var row = List.filled(qubo.size, 0.0);
+      for (var j = i; j < qubo.size; j++) {
+        var entry = qubo.getEntry(i, j);
+        if (entry != null) {
+          row[j] = entry;
+        }
+      }
+      rows.add(row);
+    }
+
+    return Matrix.fromList(rows, dtype: DType.float32);
   }
 }
--- test/qubo_embedder_test.dart
+++ test/qubo_embedder_test.dart
@@ -5,10 +5,14 @@ import 'package:ml_linalg/linalg.dart';
 
 void main() {
   test('test', () {
-    final qubo = Qubo(size: 5);
-    qubo.addEntry(0, 0, value: 0.0);
-    qubo.getEntry(1, 0);
-
+    final qubo = Qubo(size: 4);
+    qubo.addEntry(0, 0, value: 1.0);
+    qubo.addEntry(1, 1, value: 2.0);
+    qubo.addEntry(2, 2, value: 3.0);
+    qubo.addEntry(3, 3, value: 4.0);
     print(qubo);
+
+    final hamiltonian = Hamiltonian.fromQubo(qubo);
+    print(hamiltonian.matrix);
   });
 }
